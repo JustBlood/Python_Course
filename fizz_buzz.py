c636814570e676d856dc7606867b5fcf8b24d085
Just <msg.msgq@mail.ru>
2022-04-20 10:29:41 +0300

    add fizz_buzz and simple unit_test

---
+++ fizz_buzz.py
@@ -0,0 +1,10 @@
+def get_reply(number):
+    if number%3==0 and number%5==0:
+        return 'FizzBuzz'
+    elif number%3==0:
+        return 'Fizz'
+    elif number%5==0:
+        #return 'Buzz'
+        return 'Babracababra'
+    else:
+        return ''
